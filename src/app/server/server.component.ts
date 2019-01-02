e7a0a4f0425db470d3b86df5183acec44f9fca54
Trevor Hegert <thegert138@gmail.com>
2019-01-01 21:35:40 -0500

    Lesson 36 - ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,11 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-server',
-    templateUrl:  './server.component.html'
+    templateUrl:  './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
-    serverId: number = 10;
-    serverStatus: string = 'offline';
+    serverId = 10;
+    serverStatus = 'offline';
 
     constructor() {
         this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
