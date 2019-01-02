1ba52af1644a83e55225d63920c6d11e7899b514
Trevor Hegert <thegert138@gmail.com>
2019-01-01 21:29:57 -0500

    Lesson 35 complete

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -17,6 +18,7 @@ export class ServersComponent {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server ' + this.serverName + ' was created!';
   }
 
